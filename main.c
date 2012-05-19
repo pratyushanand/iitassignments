f69acb72f91c3d23cb780b62a4599af9c6e74574
Pratyush Anand <pratyush.anand@st.com>
2012-05-19 22:18:56 +0530

    just a little bit cleaning
    Signed-off-by: Pratyush Anand <pratyush.anand@st.com>

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <cv.h>
 #include <cxtypes.h>
 #include <highgui.h>
+#define MIN_AREA 1000
 
 static int32_t get_image_width(CvCapture *stream)
 {
@@ -32,41 +33,32 @@ static int32_t acquire_grayscale_image(CvCapture *stream, IplImage *gray)
 
 int main(int argc, char **argv)
 {
-	/* Your video stream */
 	CvCapture* stream = cvCaptureFromFile("../test_video/test.avi");
 	CvMemStorage* storage = cvCreateMemStorage(0);
+	int32_t width = get_image_width(stream);
+	int32_t height = get_image_height(stream);
+	int32_t stride = get_image_stride(stream);
 	CvSeq* contours = NULL;
 	CvSeq* c = NULL;
 	int count = 0;
 	int nc = 0;
-	int i;
 
-	cvNamedWindow("ORIGINAL", 1);
-//	cvNamedWindow("FG", 1);
+	cvNamedWindow("TEST", 1);
 	/* Get a model data structure */
+	/*
+	 * Library for background detection from following paper.
+	 * O. Barnich and M. Van Droogenbroeck.
+	 * ViBe: A universal background subtraction algorithm for video sequences.
+	 * In IEEE Transactions on Image Processing, 20(6):1709-1724, June 2011.
+	*/
 	vibeModel_t *model = libvibeModelNew();
-
-	/* Get the dimensions of the images of your stream 
-nb: stride is te number of bytes from the start of one row of the image  
-to the start of the next row. */
-	int32_t width = get_image_width(stream);
-	int32_t height = get_image_height(stream);
-	int32_t stride = get_image_stride(stream);
 
 	/* Allocates memory to store the input images and the segmentation maps */
 	IplImage *gray = cvCreateImage(cvSize(width, height),
 			IPL_DEPTH_8U, 1);
-	IplImage *map = cvCreateImage(cvSize(width, height),
+	IplImage *temp1 = cvCreateImage(cvSize(width, height),
 			IPL_DEPTH_8U, 1);
-	IplImage *dilate = cvCreateImage(cvSize(width, height),
-			IPL_DEPTH_8U, 1);
-	IplImage *erode = cvCreateImage(cvSize(width, height),
-			IPL_DEPTH_8U, 1);
-	IplImage *edge = cvCreateImage(cvSize(width, height),
-			IPL_DEPTH_8U, 1);
-	IplImage *seg = cvCreateImage(cvSize(width, height),
-			IPL_DEPTH_8U, 1);
-	IplImage *skelton = cvCreateImage(cvSize(width, height),
+	IplImage *temp2 = cvCreateImage(cvSize(width, height),
 			IPL_DEPTH_8U, 1);
 
 	/* Acquires your first image */
@@ -79,57 +71,47 @@ to the start of the next row. */
 	/* Processes all the following frames of your stream:
 		results are stored in "segmentation_map" */
 	while(!acquire_grayscale_image(stream, gray)){
+		/* Get FG image in temp1 */
 		libvibeModelUpdate_8u_C1R(model, gray->imageData,
-				map->imageData);
-		cvErode(map, erode, NULL, 2);
-		cvDilate(erode, dilate, NULL, 1);
-		nc = cvFindContours(dilate, storage, &contours,
+				temp1->imageData);
+		/*
+		 * Clean all small unnecessary FG objects. Get cleaned
+		 * one in temp2
+		 */
+		cvErode(temp1, temp2, NULL, 2);
+		/* Dilate it to get in proper shape */
+		cvDilate(temp2, temp1, NULL, 1);
+		/*
+		 * Find out all moving contours , so basically segment
+		 * it out. Create separte image for each moving object.
+		 */
+		 nc = cvFindContours(temp1, storage, &contours,
 				sizeof(CvContour), CV_RETR_EXTERNAL,
 				CV_CHAIN_APPROX_NONE, cvPoint(0, 0));
 		for (c = contours; c != NULL; c = c->h_next) {
 			float area = cvContourArea(c, CV_WHOLE_SEQ, 0);
 			char name[30];
 
-			if (area > 1000) {
-				cvZero(skelton);
-				cvDrawContours(skelton, c, cvScalar(255, 255, 255, 0), 
+			/*
+			 * choose only if moving object area is greater
+			 * than MIN_AREA square pixel
+			 */
+			if (area > MIN_AREA) {
+				cvZero(temp2);
+				cvDrawContours(temp2, c, cvScalar(255, 255, 255, 0), 
 						cvScalar(0, 0, 0, 0), -1,
 						CV_FILLED, 8, cvPoint(0, 0));
-//				sprintf(name, "out%d.jpg", count++);
-//				cvSaveImage(name, skelton, 0);
 				cvWaitKey(30);
-				cvShowImage("ORIGINAL", skelton);
+				cvShowImage("TEST", temp2);
 			}
 		}
-#if 0
-		while (nc--) {
-			for (i = 0; i < contours->total; i++) {
-				CvPoint* p = CV_GET_SEQ_ELEM(CvPoint, c,
-						i);
-				*((uchar*) (skelton->imageData +
-				 p->y * skelton->widthStep) + p->x) =
-					255;
-				printf("%d %d %d\n", p->x, p->y, nc);
-			}
-			c = c->h_next;
-			if (!c)
-				break;
-			cvShowImage("ORIGINAL", skelton);
-		}
-		printf("\n");
-#endif
-//		cvShowImage("FG", edge);
-		cvWaitKey(30);
 	}
 
 	/* Cleanup allocated memory */
 	libvibeModelFree(model);
 	cvReleaseImage(&gray);
-	cvReleaseImage(&map);
-	cvReleaseImage(&erode);
-	cvReleaseImage(&dilate);
-	cvReleaseImage(&seg);
-	cvReleaseImage(&skelton);
+	cvReleaseImage(&temp1);
+	cvReleaseImage(&temp2);
 	cvReleaseMemStorage(&storage);
 
 	return(0);
