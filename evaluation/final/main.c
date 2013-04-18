6c31b1c6bdaa0510431d3bb51d1b6f60fd160d47
Pratyush Anand <pratyush.anand@st.com>
2013-04-19 00:07:28 +0530

    temp
    Signed-off-by: Pratyush Anand <pratyush.anand@st.com>

--- evaluation/final/main.c
+++ evaluation/final/main.c
@@ -48,6 +48,19 @@ filter()
 #define MAX_OBJECT	20
 #define MAX_BOUNDARY_POINT 200
 #define MAX_TRACK_HISTORY 100
+enum obj_type {
+	TYPE_UNDEFINED = 0,
+	TYPE_HUMAN = 1,
+};
+struct object_class {
+	/* type of object */
+	enum obj_type type;
+	/* level of confidence with which type has been identified */
+	int confidence;
+	int x;
+	int y;
+};
+
 struct tracker{
 	int theta1[MAX_TRACK_HISTORY];
 	int theta2[MAX_TRACK_HISTORY];
@@ -56,21 +69,194 @@ struct tracker{
 	int cx[MAX_TRACK_HISTORY];
 	int cy[MAX_TRACK_HISTORY];
 	int cur;
+	struct object_class object;
 	LIST_ENTRY(tracker) list;
 };
 
 static LIST_HEAD(trackerhead, tracker) tracker_head;
 
-void update_tracker(int theta1, int theta2, int di, int hy, int
-		cx, int cy)
+void update_tracker(CvSeq *c, int width, int height, struct object_class
+		*object)
 {
 	int found = 0;
 	struct tracker *node;
+	CvMat* mat = cvCreateMat(1, width * height, CV_32FC1);
+	CvMat* smat = cvCreateMat(1, width * height, CV_32FC1);
+	CvRect rect;
+	CvPoint skel_pt_all[MAX_BOUNDARY_POINT];
+	CvPoint skel_pt_final[5];
+	double theta1, theta2;
+	float di, di1, delta, delta1;
+	int i, j, cx, cy, hy, wx, dmin, dmin_prev;
+	IplImage *temp1 = cvCreateImage(cvSize(width, height), IPL_DEPTH_8U, 1);
+#if 0
+	cvNamedWindow("SEPRATED_CONTOUR", 1);
+	cvMoveWindow("SEPRATED_CONTOUR", 3 * width, 0);
+	cvNamedWindow("DI", 1);
+	cvMoveWindow("DI", width, 2 * height);
+#endif
+	cvNamedWindow("SKELETON", 1);
+	cvMoveWindow("SKELETON", 0, 2 * height);
 
-//	printf("%d %d %d %d %d %d\n", theta1, theta2, di, hy, cx, cy);
+	cvWaitKey(0);
+	cvZero(temp1);
+	for (i = 0; i < c->total; i++) {
+		CvPoint* p = CV_GET_SEQ_ELEM(CvPoint, c,
+				i);
+		*((uchar*) (temp1->imageData +
+					p->y * temp1->widthStep) + p->x) = 255;
+	}
+//	cvShowImage("SEPRATED_CONTOUR", temp1);
+	/* calculate centroid */
+	cx = 0;
+	cy = 0;
+	for (i = 0; i < c->total; i++) {
+		CvPoint* p = CV_GET_SEQ_ELEM(CvPoint, c,
+				i);
+		cx += p->x;
+		cy += p->y;
+	}
+	cx /= c->total;
+	cy /= c->total;
+
+	/* calculate distance vector */
+	cvZero(mat);
+	for (i = 0; i < c->total; i++) {
+		CvPoint* p = CV_GET_SEQ_ELEM(CvPoint, c,
+				i);
+		*((float*)CV_MAT_ELEM_PTR(*mat, 0, i)) =
+			sqrt(pow((p->x - cx), 2) +
+					pow((p->y - cy), 2));
+	}
+	/* Low pass filter it */
+	cvSmooth(mat, smat, CV_GAUSSIAN, 39, 1, 0, 0);
+#if 0
+	/*plot disatance vector */
+	dii = temp1;
+	cvZero(dii);
+	for (i = 0; i < c->total; i++) {
+		int x = CV_MAT_ELEM(*smat, float, 0, i);
+		*((uchar*) (dii->imageData +
+					(height - x) * dii->widthStep) + i) = 255;
+	}
+//	cvShowImage("DI", dii);
+#endif
+	/* find extream points */
+	cvZero(temp1);
+
+	di = CV_MAT_ELEM(*smat, float, 0, 0);
+	di1 = CV_MAT_ELEM(*smat, float, 0, 1);
+	delta = di1 - di;
+	j = 0;
+	for (i = 1; i < c->total; i++) {
+		di = CV_MAT_ELEM(*smat, float, 0, i);
+		di1 = CV_MAT_ELEM(*smat, float, 0,
+				(i + 1) % c->total);
+		delta1 = di1 - di;
+		if ((delta >= 0 && delta1 < 0) ||
+				(delta <= 0 && delta1 > 0)) {
+			CvPoint* p = CV_GET_SEQ_ELEM(
+					CvPoint, c, i);
+			skel_pt_all[j].x = p->x;
+			skel_pt_all[j++].y = p->y;
+			if (j == MAX_BOUNDARY_POINT) {
+				printf("Error with parameters\n");
+				goto exit;
+			}
+			//						cvLine(temp1, *p, cvPoint(cx, cy),
+			//							cvScalar(255, 255, 255, 0), 1, 8, 0);
+		}
+		delta = delta1;
+	}
+	rect=cvBoundingRect (c, 0);
+	hy = rect.height;
+	wx = rect.width;
+	/* Centroid of final skeleton */
+	skel_pt_final[0].x = cx;
+	skel_pt_final[0].y = cy;
+	/* top left point of skeleton */
+	dmin_prev = sqrt(pow(rect.height, 2) +
+			pow(rect.width, 2));
+	for (i = 0; i < j; i++) {
+		dmin = sqrt(pow((rect.x - skel_pt_all[i].x), 2) +
+				pow((rect.y - skel_pt_all[i].y), 2));
+		if (dmin < dmin_prev) {
+			skel_pt_final[1].x =
+				skel_pt_all[i].x;
+			skel_pt_final[1].y =
+				skel_pt_all[i].y;
+			dmin_prev = dmin;
+		}
+	}
+	/* top right point of skeleton */
+	dmin_prev = sqrt(pow(rect.height, 2) +
+			pow(rect.width, 2));
+	for (i = 0; i < j; i++) {
+		dmin = sqrt(pow((rect.x + rect.width - skel_pt_all[i].x), 2) +
+				pow((rect.y - skel_pt_all[i].y), 2));
+		if (dmin < dmin_prev) {
+			skel_pt_final[2].x =
+				skel_pt_all[i].x;
+			skel_pt_final[2].y =
+				skel_pt_all[i].y;
+			dmin_prev = dmin;
+		}
+	}
+	/* bottom right point of skeleton */
+	dmin_prev = sqrt(pow(rect.height, 2) +
+			pow(rect.width, 2));
+	for (i = 0; i < j; i++) {
+		dmin = sqrt(pow((rect.x + rect.width - skel_pt_all[i].x), 2) +
+				pow((rect.y + rect.height - skel_pt_all[i].y), 2));
+		if (dmin < dmin_prev) {
+			skel_pt_final[3].x =
+				skel_pt_all[i].x;
+			skel_pt_final[3].y =
+				skel_pt_all[i].y;
+			dmin_prev = dmin;
+		}
+	}
+	/* bottom left point of skeleton */
+	dmin_prev = sqrt(pow(rect.height, 2) +
+			pow(rect.width, 2));
+	for (i = 0; i < j; i++) {
+		dmin = sqrt(pow((rect.x - skel_pt_all[i].x), 2) +
+				pow((rect.y + rect.height - skel_pt_all[i].y), 2));
+		if (dmin < dmin_prev) {
+			skel_pt_final[4].x =
+				skel_pt_all[i].x;
+			skel_pt_final[4].y =
+				skel_pt_all[i].y;
+			dmin_prev = dmin;
+		}
+	}
+#if 0
+	for (i = 0; i < 5; i++) {
+		*((uchar*) (temp1->imageData +
+					skel_pt_final[i].y * skel->widthStep) + skel_pt_final[i].x) = 255;
+	}
+#endif
+	cvLine(temp1, skel_pt_final[0], skel_pt_final[1],
+			cvScalar(255, 0, 0, 0), 1, CV_AA,0);
+	cvLine(temp1, skel_pt_final[0], skel_pt_final[2],
+			cvScalar(255, 0, 0, 0), 1, CV_AA,0);
+	cvLine(temp1, skel_pt_final[0], skel_pt_final[3],
+			cvScalar(255, 0, 0, 0), 1, CV_AA,0);
+	cvLine(temp1, skel_pt_final[0], skel_pt_final[4],
+			cvScalar(255, 0, 0, 0), 1, CV_AA,0);
+	cvShowImage("SKELETON", temp1);
+	theta1 = atan2(skel_pt_final[2].y - skel_pt_final[0].y, skel_pt_final[2].x - skel_pt_final[0].x) * 180.0 / CV_PI -
+		atan2(skel_pt_final[1].y - skel_pt_final[0].y, skel_pt_final[1].x - skel_pt_final[0].x) * 180.0 / CV_PI;
+	theta2 = atan2(skel_pt_final[4].y - skel_pt_final[0].y, skel_pt_final[4].x - skel_pt_final[0].x) * 180.0 / CV_PI -
+		atan2(skel_pt_final[3].y - skel_pt_final[0].y, skel_pt_final[3].x - skel_pt_final[0].x) * 180.0 / CV_PI;
+	di = sqrt(pow(((skel_pt_final[3].x + skel_pt_final[4].x)/2 - cx), 2) +
+			pow(((skel_pt_final[3].y + skel_pt_final[4].y)/2 - cy), 2));
+	printf("%f %f %f %d %d %d %d %d %d %d\n", theta1, theta2, di, hy, cx, cy,
+			skel_pt_final[3].x, skel_pt_final[3].y,
+			skel_pt_final[4].x, skel_pt_final[4].y);
 	LIST_FOREACH(node, &tracker_head, list) {
-//		printf("Matching object\n");
-		if (abs(node->cx[node->cur] - cx) < 10 && abs(node->cy[node->cur] -cy) < 10) {
+		//		printf("Matching object\n");
+		if (abs(node->cx[node->cur] - cx) < 10 && abs(node->cy[node->cur] - cy) < 10) {
 			node->cur++;
 			node->theta1[node->cur] = theta1;
 			node->theta2[node->cur] = theta2;
@@ -79,11 +265,12 @@ void update_tracker(int theta1, int theta2, int di, int hy, int
 			node->cx[node->cur] = cx;
 			node->cy[node->cur] = cy;
 			found = 1;
-			printf("object matched\n");
+//			printf("object matched\n");
+			break;
 		}
 	}
 	if (!found) {
-		printf("object not matched\n");
+//		printf("object not matched\n");
 		/* this is a new object */
 		node = (struct tracker *)malloc(sizeof(*node));
 		node->theta1[0] = theta1;
@@ -95,6 +282,22 @@ void update_tracker(int theta1, int theta2, int di, int hy, int
 		node->cur = 0;
 		LIST_INSERT_HEAD(&tracker_head, node, list);
 	}
+	node->object.type = TYPE_UNDEFINED;
+	node->object.confidence = 0;
+	if (theta1 >= 5 && theta1 <= 20) {
+		node->object.type = TYPE_HUMAN;
+		node->object.confidence = 60;
+	}
+	if (theta2 == 0 && ((abs(di - hy / 2)) / di) < 0.05)
+		node->object.confidence += 10;
+	object->type = node->object.type;
+	object->confidence = node->object.confidence;
+	object->x = cx;
+	object->y = cy;
+	//printf("%f\t%f\t%f\t%d\n", theta1, theta2, di, hy);
+exit:
+	cvReleaseMat(&mat);
+	cvReleaseMat(&smat);
 }
 
 int main(int argc, char **argv)
@@ -106,30 +309,20 @@ int main(int argc, char **argv)
 	int32_t stride = get_image_stride(stream);
 	CvSeq* contours = NULL;
 	CvSeq* c = NULL;
-	IplImage *gray, *temp1, *temp2, *map, *skel, *eroded,
+	IplImage *gray, *temp1, *temp2, *map, *eroded,
 		 *dilated, *dii;
-	float area, di, di1, delta, delta1;
-	int frame = 0, object = 0, i, j, cx, cy, hy, wx, dmin, dmin_prev;
-	CvMat* mat = cvCreateMat(1, width * height, CV_32FC1);
-	CvMat* smat = cvCreateMat(1, width * height, CV_32FC1);
-	CvRect rect;
-	IplConvKernel *element;
-	CvPoint skel_pt_all[MAX_BOUNDARY_POINT];
-	CvPoint skel_pt_final[5];
-	double theta1, theta2;
+	int frame;
+	float area;
+	struct object_class object;
 
 	LIST_INIT(&tracker_head);
+#if 0
 	cvNamedWindow("GRAY", 1);
 	cvNamedWindow("FG", 1);
 	cvMoveWindow("FG", width, 0);
 	cvNamedWindow("CLEANED", 1);
 	cvMoveWindow("CLEANED", 2 * width, 0);
-	cvNamedWindow("SEPRATED_CONTOUR", 1);
-	cvMoveWindow("SEPRATED_CONTOUR", 3 * width, 0);
-	cvNamedWindow("DI", 1);
-	cvMoveWindow("DI", width, 2 * height);
-	cvNamedWindow("SKELTON", 1);
-	cvMoveWindow("SKELTON", 0, 2 * height);
+#endif
 	/* Get a model data structure */
 	/*
 	 * Library for background detection from following paper.
@@ -150,7 +343,6 @@ int main(int argc, char **argv)
 	/* Allocates the model and initialize it with the first image */
 	libvibeModelAllocInit_8u_C1R(model, (const uint8_t *)gray->imageData, width, height,
 			stride);
-	element = cvCreateStructuringElementEx(5, 5, 2, 2, CV_SHAPE_RECT, NULL);
 
 	/* Processes all the following frames of your stream:
 		results are stored in "segmentation_map" */
@@ -159,13 +351,12 @@ int main(int argc, char **argv)
 		if (frame < 200)
 			continue;
 		//printf("%d\n", frame);
-		object = 0;
-		cvShowImage("GRAY", gray);
+		//cvShowImage("GRAY", gray);
 		/* Get FG image in temp1 */
 		map = temp1;
 		libvibeModelUpdate_8u_C1R(model, (const uint8_t *)gray->imageData,
 				(uint8_t *)map->imageData);
-		cvShowImage("FG", map);
+		//cvShowImage("FG", map);
 		/*
 		 * Clean all small unnecessary FG objects. Get cleaned
 		 * one in temp2
@@ -175,7 +366,7 @@ int main(int argc, char **argv)
 		/* Dilate it to get in proper shape */
 		dilated = temp1;
 		cvDilate(eroded, dilated, NULL, 1);
-		cvShowImage("CLEANED", dilated);
+		//cvShowImage("CLEANED", dilated);
 		/*
 		 * Find out all moving contours , so basically segment
 		 * it out. Create separte image for each moving object.
@@ -192,181 +383,16 @@ int main(int argc, char **argv)
 			 * than MIN_AREA square pixel
 			 */
 			if (area > MIN_AREA) {
-				cvWaitKey(0);
-//				if (frame == 551)
-//					cvWaitKey(0);
-				/*
-				 * If bandwidth is high then send data
-				 * at this stage only. No need to
-				 * skeltonize
-				 */
-				cvZero(temp1);
-				for (i = 0; i < c->total; i++) {
-					CvPoint* p = CV_GET_SEQ_ELEM(CvPoint, c,
-							i);
-					*((uchar*) (temp1->imageData +
-					p->y * temp1->widthStep) + p->x) = 255;
-				}
-				cvShowImage("SEPRATED_CONTOUR", temp1);
-				/* calculate centroid */
-				cx = 0;
-				cy = 0;
-				for (i = 0; i < c->total; i++) {
-					CvPoint* p = CV_GET_SEQ_ELEM(CvPoint, c,
-							i);
-					cx += p->x;
-					cy += p->y;
-				}
-				cx /= c->total;
-				cy /= c->total;
-
-#if 1
-				/* calculate distance vector */
-				cvZero(mat);
-				for (i = 0; i < c->total; i++) {
-					CvPoint* p = CV_GET_SEQ_ELEM(CvPoint, c,
-							i);
-					*((float*)CV_MAT_ELEM_PTR(*mat, 0, i)) =
-						sqrt(pow((p->x - cx), 2) +
-								pow((p->y - cy), 2));
-				}
-				/* Low pass filter it */
-				cvSmooth(mat, smat, CV_GAUSSIAN, 39, 1, 0, 0);
-#if 0
-				/*plot disatance vector */
-				dii = temp1;
-				cvZero(dii);
-				for (i = 0; i < c->total; i++) {
-					int x = CV_MAT_ELEM(*smat, float, 0, i);
-					*((uchar*) (dii->imageData +
-					(height - x) * dii->widthStep) + i) = 255;
-				}
-				cvShowImage("DI", dii);
-#endif
-				/* find extream points */
-				skel = temp1;
-				cvZero(skel);
-
-				di = CV_MAT_ELEM(*smat, float, 0, 0);
-				di1 = CV_MAT_ELEM(*smat, float, 0, 1);
-				delta = di1 - di;
-				j = 0;
-				for (i = 1; i < c->total; i++) {
-					di = CV_MAT_ELEM(*smat, float, 0, i);
-					di1 = CV_MAT_ELEM(*smat, float, 0,
-							(i + 1) % c->total);
-					delta1 = di1 - di;
-					if ((delta >= 0 && delta1 < 0) ||
-						(delta <= 0 && delta1 > 0)) {
-						CvPoint* p = CV_GET_SEQ_ELEM(
-								CvPoint, c, i);
-						skel_pt_all[j].x = p->x;
-						skel_pt_all[j++].y = p->y;
-						if (j == MAX_BOUNDARY_POINT) {
-							printf("Error with parameters\n");
-							goto exit;
-						}
-//						cvLine(skel, *p, cvPoint(cx, cy),
-//							cvScalar(255, 255, 255, 0), 1, 8, 0);
-					}
-					delta = delta1;
-				}
-				rect=cvBoundingRect (c, 0);
-				hy = rect.height;
-				wx = rect.width;
-				/* Centroid of final skeleton */
-				skel_pt_final[0].x = cx;
-				skel_pt_final[0].y = cy;
-				/* top left point of skeleton */
-				dmin_prev = sqrt(pow(rect.height, 2) +
-						pow(rect.width, 2));
-				for (i = 0; i < j; i++) {
-					dmin = sqrt(pow((rect.x - skel_pt_all[i].x), 2) +
-								pow((rect.y - skel_pt_all[i].y), 2));
-					if (dmin < dmin_prev) {
-						skel_pt_final[1].x =
-							skel_pt_all[i].x;
-						skel_pt_final[1].y =
-							skel_pt_all[i].y;
-						dmin_prev = dmin;
-					}
-				}
-				/* top right point of skeleton */
-				dmin_prev = sqrt(pow(rect.height, 2) +
-						pow(rect.width, 2));
-				for (i = 0; i < j; i++) {
-					dmin = sqrt(pow((rect.x + rect.width - skel_pt_all[i].x), 2) +
-								pow((rect.y - skel_pt_all[i].y), 2));
-					if (dmin < dmin_prev) {
-						skel_pt_final[2].x =
-							skel_pt_all[i].x;
-						skel_pt_final[2].y =
-							skel_pt_all[i].y;
-						dmin_prev = dmin;
-					}
-				}
-				/* bottom right point of skeleton */
-				dmin_prev = sqrt(pow(rect.height, 2) +
-						pow(rect.width, 2));
-				for (i = 0; i < j; i++) {
-					dmin = sqrt(pow((rect.x + rect.width - skel_pt_all[i].x), 2) +
-								pow((rect.y + rect.height - skel_pt_all[i].y), 2));
-					if (dmin < dmin_prev) {
-						skel_pt_final[3].x =
-							skel_pt_all[i].x;
-						skel_pt_final[3].y =
-							skel_pt_all[i].y;
-						dmin_prev = dmin;
-					}
-				}
-				/* bottom left point of skeleton */
-				dmin_prev = sqrt(pow(rect.height, 2) +
-						pow(rect.width, 2));
-				for (i = 0; i < j; i++) {
-					dmin = sqrt(pow((rect.x - skel_pt_all[i].x), 2) +
-								pow((rect.y + rect.height - skel_pt_all[i].y), 2));
-					if (dmin < dmin_prev) {
-						skel_pt_final[4].x =
-							skel_pt_all[i].x;
-						skel_pt_final[4].y =
-							skel_pt_all[i].y;
-						dmin_prev = dmin;
-					}
-				}
-#if 0
-				for (i = 0; i < 5; i++) {
-					*((uchar*) (skel->imageData +
-						skel_pt_final[i].y * skel->widthStep) + skel_pt_final[i].x) = 255;
-				}
-#endif
-				cvLine(skel, skel_pt_final[0], skel_pt_final[1],
-						cvScalar(255, 0, 0, 0), 1, CV_AA,0);
-				cvLine(skel, skel_pt_final[0], skel_pt_final[2],
-						cvScalar(255, 0, 0, 0), 1, CV_AA,0);
-				cvLine(skel, skel_pt_final[0], skel_pt_final[3],
-						cvScalar(255, 0, 0, 0), 1, CV_AA,0);
-				cvLine(skel, skel_pt_final[0], skel_pt_final[4],
-						cvScalar(255, 0, 0, 0), 1, CV_AA,0);
-				cvShowImage("SKELTON", skel);
-				theta1 = atan2(skel_pt_final[2].y - skel_pt_final[0].y, skel_pt_final[2].x - skel_pt_final[0].x) * 180.0 / CV_PI -
-					atan2(skel_pt_final[1].y - skel_pt_final[0].y, skel_pt_final[1].x - skel_pt_final[0].x) * 180.0 / CV_PI;
-				theta2 = atan2(skel_pt_final[4].y - skel_pt_final[0].y, skel_pt_final[4].x - skel_pt_final[0].x) * 180.0 / CV_PI -
-					atan2(skel_pt_final[3].y - skel_pt_final[0].y, skel_pt_final[3].x - skel_pt_final[0].x) * 180.0 / CV_PI;
-				di = sqrt(pow(((skel_pt_final[3].x + skel_pt_final[4].x)/2 - cx), 2) +
-								pow(((skel_pt_final[3].y + skel_pt_final[4].y)/2 - cy), 2));
-				update_tracker((int)theta1, (int)theta2, (int)di, hy, cx, cy);
-#endif
+				update_tracker(c, width, height, &object);
+				//printf("%d\t%d\t%d\t%d\n", object.type, object.confidence, object.x, object.y);
 			}
 		}
 				gray = temp2;
 	}
-exit:
 	/* Cleanup allocated memory */
 	libvibeModelFree(model);
 	cvReleaseImage(&temp1);
 	cvReleaseImage(&temp2);
-	cvReleaseMat(&mat);
-	cvReleaseMat(&smat);
 	cvReleaseMemStorage(&storage);
 
 	return(0);
